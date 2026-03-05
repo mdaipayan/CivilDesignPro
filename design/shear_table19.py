272f8e857fc250e3e65d745423dec5c6d1e6f321
mdaipayan <49827916+mdaipayan@users.noreply.github.com>
2026-03-05 21:02:00 +0530

    Update shear_table19.py

--- design/shear_table19.py
+++ design/shear_table19.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+table19 = pd.read_csv("database/shear_table19.csv")
+
+def get_tau_c(p, fck):
+
+    row = table19.iloc[(table19['p']-p).abs().argsort()[:1]]
+
+    return row[f"fck{fck}"].values[0]
